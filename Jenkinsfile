93532468152860e56db4f278aea1cbee4d42dc75
Pushkar Pawar <pushkar@vadelabs.com>
2022-11-25 01:56:14 +0530

    "Code refactor"

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,22 +12,10 @@ pipeline {
                 sh "python3 multiply.py"
             }
         }
-     stage('Test Code Success') {
+     stage('Test Code') {
             steps {
                 sh "chmod u+x Test.py"
                 sh "python3 Test.py"
-                sh "chmod u+x Test1.py"
-                sh "python3 Test1.py"
-                sh "chmod u+x Test2.py"
-                sh "python3 Test2.py"
-            }
-        }
-        stage('Test Code Failure') {
-            steps {
-                sh "chmod u+x Test3.py"
-                sh "python3 Test3.py"
-                sh "chmod u+x Test4.py"
-                sh "python3 Test4.py"
             }
         }
     } 
--- Test.py
+++ Test.py
@@ -12,5 +12,40 @@ class TestSum(unittest.TestCase):
         result = Mul(data)
         self.assertEqual(result, 600)
 
+    def test_list_int1(self):
+        """
+        Test case to add two numbers
+        """
+        data = [1,2,3,4]
+        result = Mul(data)
+        self.assertEqual(result, 24)
+    
+    def test_list_int2(self):
+        """
+        Test case to add two numbers
+        """
+        data = [20, 0,3,12]
+        result = Mul(data)
+        self.assertEqual(result, 0)
+
+    def test_list_int3(self):
+        """
+        Test case to add two numbers
+        """
+        data = [20, 30]
+        result = Mul(data)
+        self.assertEqual(result, 0)
+
+    def test_list_int4(self):
+        """
+        Test case to add two numbers
+        """
+        data = [20, 30]
+        result = Mul(data)
+        self.assertEqual(result, 6)
+
+    
+    
+
 if __name__ == '__main__':
     unittest.main()
